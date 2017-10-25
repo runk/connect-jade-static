0082beeedb81eabd0961f80ef302a7762ef6b1f3
Tuan Nguyen <lightbringer1991@yahoo.com>
2017-10-25 16:24:19 +1100

    upgrade to pug

--- index.js
+++ index.js
@@ -1,111 +1,117 @@
-var path = require('path'),
-  fs = require('fs'),
-  jade = require('jade'),
-  url = require('url'),
-  assert = require('assert');
+/**
+ * based on connect-jade-static (https://github.com/runk/connect-jade-static)
+ * connect-pug-static is not completed and cannot be used. This is a quick fix to turn connect-jade-static to handle
+ * pug files
+ */
 
-var defaultOptions = {
-  // Jade options
-  jade: {},
-  // Serve index.jade if http://someurl/example/ is requested
+const _ = require('lodash');
+const path = require('path');
+const fs = require('fs'); // eslint-disable-line id-length
+const pug = require('pug');
+const url = require('url');
+const assert = require('assert');
+
+const defaultOptions = {
+  // Pug options
+  pug: {},
+  // Serve index.pug if http://someurl/example/ is requested
   serveIndex: true,
-  // Valid jade template extensions
-  ext: ['.jade'],
+  // Valid pug template extensions
+  ext: ['.pug'],
   // Allowed request extension
-  allowedExt: ['.jade', '.htm', '.html'],
+  allowedExt: ['.pug', '.htm', '.html'],
   // Header for Cache-Control: max-age=0
-  maxAge: 0
+  maxAge: 0,
 };
 
-module.exports = function(opts) {
 
-  if (!opts.baseDir)
+module.exports = function (opts) {
+  if (!opts.baseDir) {
     throw new Error('baseDir should be set');
+  }
 
-  if (!opts.baseUrl)
+  if (!opts.baseUrl) {
     throw new Error('baseUrl should be set');
+  }
 
-  opts = module.exports.getDefaultOptions(opts);
+  const newOpts = module.exports.getDefaultOptions(opts);
 
-  return function(req, res, next) {
-
-    if (req.originalUrl.indexOf(opts.baseUrl) !== 0)
+  return function (req, res, next) {
+    if (!_.startsWith(req.originalUrl, newOpts.baseUrl)) {
       return next();
-
-    var filepath = module.exports.getTplPath(req.originalUrl, opts);
-
-    if (!filepath)
-      return next();
-
-    if (filepath.indexOf(opts.baseDir) !== 0)
-      return res.sendStatus(403);
-
-    fs.stat(filepath, function(err, stats) {
-      if (err)
-        return next(err);
-
-      if (!stats.isFile())
-        return next();
-
-      jade.renderFile(filepath, opts.jade, function renderFile(err, html) {
-        if (err)
-          return next(err);
-
-        res.setHeader('Content-Length', Buffer.byteLength(html));
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        var now = +new Date();
-        var expires = new Date(now + opts.maxAge);
-        res.setHeader('Cache-Control', 'max-age=' + opts.maxAge);
-        res.setHeader('Expires', expires.toGMTString());
-        return res.end(html);
-      });
-    });
-  };
-
-};
-
-module.exports.getDefaultOptions = function (opts) {
-  opts = opts || {};
-  Object.keys(defaultOptions).forEach(function (optionName) {
-    if (opts[optionName] === undefined) {
-      opts[optionName] = defaultOptions[optionName];
     }
+
+    const filePath = module.exports.getTplPath(req.originalUrl, newOpts);
+
+    if (!filePath) {
+      return next();
+    }
+
+    if (!_.startsWith(filePath, newOpts.baseDir)) {
+      return res.sendStatus(403);
+    }
+
+    fs.stat(filePath, (err, stats) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (!stats.isFile()) {
+      return next();
+    }
+
+    pug.renderFile(filePath, newOpts.pug, (renderErr, html) => {
+      if (renderErr) {
+        return next(renderErr);
+      }
+
+      res.setHeader('Content-Length', Buffer.byteLength(html));
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    const now = +new Date();
+    const expires = new Date(now + newOpts.maxAge);
+    res.setHeader('Cache-Control', `max-age=${newOpts.maxAge}`);
+    res.setHeader('Expires', expires.toGMTString());
+    return res.end(html);
   });
-  return opts;
+  });
+  };
 };
 
 
-module.exports.getTplPath = function(requestUrl, opts) {
-  opts = module.exports.getDefaultOptions(opts);
-  assert(opts.ext && Array.isArray(opts.ext), 'opts.ext should be provided and be an array');
-  assert(opts.baseDir, 'opts.baseDir should be provided');
+module.exports.getDefaultOptions = (opts = {}) => _.assign({}, defaultOptions, opts);
 
-  var parsed = url.parse(requestUrl);
-  var pathname = parsed.pathname.replace(opts.baseUrl, '');
-  var requestedExt = path.extname(pathname);
-  var pathnameWithoutExt = pathname.substr(0, pathname.length - requestedExt.length);
 
-  if (opts.serveIndex && requestedExt === '') {
+module.exports.getTplPath = (requestUrl, opts) => {
+  const newOpts = module.exports.getDefaultOptions(opts);
+  assert(newOpts.ext && _.isArray(newOpts.ext), 'newOpts.ext should be provided and be an array');
+  assert(newOpts.baseDir, 'newOpts.baseDir should be provided');
+
+  const parsed = url.parse(requestUrl);
+  const pathname = parsed.pathname.replace(newOpts.baseUrl, '');
+  let requestedExt = path.extname(pathname);
+  let pathnameWithoutExt = pathname.substr(0, pathname.length - requestedExt.length);
+
+  if (newOpts.serveIndex && requestedExt === '') {
     // Handle http://example.com/example-path
-    if(pathname.substr(-1) !== '/') {
+    if (pathname.substr(-1) !== '/') {
       pathnameWithoutExt += '/';
     }
     // Handle http://example.com/example-path/
     pathnameWithoutExt += 'index';
-    requestedExt = opts.allowedExt[0];
+    requestedExt = newOpts.allowedExt[0];
   }
 
-  // Allow only .html .htm .jade ...
-  if (opts.allowedExt.indexOf(requestedExt) === -1) {
+  // Allow only .html .htm .pug ...
+  if (!_.includes(newOpts.allowedExt, requestedExt)) {
     return null;
   }
 
   // Search for an existing template file
-  for (var i = 0; i < opts.ext.length; i++) {
-    var ext = opts.ext[i];
-    var filepath = path.join(opts.baseDir, pathnameWithoutExt + ext);
-    if (fs.existsSync(filepath)) {
-      return filepath;
+  for (let idx = 0; idx < newOpts.ext.length; idx++) {
+    const ext = newOpts.ext[idx];
+    const filePath = path.join(newOpts.baseDir, pathnameWithoutExt + ext);
+    if (fs.existsSync(filePath)) {
+      return filePath;
     }
   }
 
--- test/index.js
+++ test/index.js
@@ -1,82 +1,80 @@
-var assert = require('assert'),
-  path = require('path'),
-  cjs = require('../index');
+const assert = require('assert');
+const path = require('path');
+const connectPugStatic = require('../index');
 
 
-describe('connect-jade-static', function() {
-
-  var next = function(err) {
+describe('connectPugStatic', () => {
+  let middleware = null;
+  const baseDir = path.join(__dirname, 'views');
+  const next = () => {
     throw new Error('next() shouldn\'t be called');
   };
 
-  describe('middleware()', function() {
+  beforeEach(() => {
+    middleware = connectPugStatic({ baseUrl: '/views', baseDir });
+  });
 
-    it('should raise error when insufficient params provided', function() {
-      assert.throws(function() {
-        cjs({ baseUrl: '/' });
-      });
-
-      assert.throws(function() {
-        cjs({ baseDir: '/' });
-      });
-
-      assert.throws(function() {
-        cjs(null);
-      });
+  describe('middleware()', () => {
+    it('should raise error when insufficient params provided', () => {
+      assert.throws(() => { connectPugStatic({ baseUrl: '/' }); });
+      assert.throws(() => { connectPugStatic({ baseDir: '/' }); });
+      assert.throws(() => { connectPugStatic(null); });
     });
 
-    it('should serve jade file', function(done) {
-      var mw = cjs({ baseUrl: '/views', baseDir: path.join(__dirname, 'views') });
-      var headers = {};
-      var req = { originalUrl: '/views/tpl.html' };
-      var res = {
-        end: function(html) {
-          // otherwise jade tries to catch this error :/
-          process.nextTick(function() {
+    it('should serve pug file', (done) => {
+      const headers = {};
+      const req = { originalUrl: '/views/tpl.html' };
+      const res = {
+        end: (html) => {
+          // otherwise pug tries to catch this error :/
+          process.nextTick(() => {
             assert.equal(html, '<h1>Hello</h1><ul><li>aaa</li><li>bbb</li><li>ccc</li></ul>');
             assert.equal(headers['Content-Length'], 59);
             assert.equal(headers['Content-Type'], 'text/html; charset=utf-8');
             done();
           });
         },
-        setHeader: function(k, v) {
-          headers[k] = v;
-        }
+        setHeader: (key, value) => {
+          headers[key] = value;
+        },
       };
 
-      mw(req, res, next);
+      middleware(req, res, next);
     });
 
-    it('should serve index file', function (done) {
-      var mw = cjs({ baseUrl: '/views', baseDir: path.join(__dirname, 'views') });
-      var headers = {};
-      var req = { originalUrl: '/views/foo/' };
-      var res = {
-        end: function (html) {
-          // otherwise jade tries to catch this error :/
-          process.nextTick(function () {
+    it('should serve index file', (done) => {
+      const headers = {};
+      const req = { originalUrl: '/views/foo/' };
+      const res = {
+        end: (html) => {
+          // otherwise pug tries to catch this error :/
+          process.nextTick(() => {
             assert.equal(html, '<h1>index</h1>');
             assert.equal(headers['Content-Length'], 14);
             assert.equal(headers['Content-Type'], 'text/html; charset=utf-8');
             done();
           });
         },
-        setHeader: function (k, v) {
-          headers[k] = v;
-        }
+        setHeader: (key, value) => {
+          headers[key] = value;
+        },
       };
 
-      mw(req, res, next);
+      middleware(req, res, next);
     });
 
-    it('should support jade options', function(done) {
-      var mw = cjs({ baseUrl: '/views', baseDir: path.join(__dirname, 'views'), jade: { pretty: true } });
-      var headers = {};
-      var req = { originalUrl: '/views/tpl.html' };
-      var res = {
-        end: function(html) {
-          // otherwise jade tries to catch this error :/
-          process.nextTick(function() {
+    it('should support pug options', (done) => {
+      middleware = connectPugStatic({
+        baseUrl: '/views',
+        baseDir: path.join(__dirname, 'views'),
+        pug: { pretty: true },
+      });
+      const headers = {};
+      const req = { originalUrl: '/views/tpl.html' };
+      const res = {
+        end: (html) => {
+          // otherwise pug tries to catch this error :/
+          process.nextTick(() => {
             assert.equal(html,
               '\n<h1>Hello</h1>\n' +
               '<ul>\n' +
@@ -89,133 +87,138 @@ describe('connect-jade-static', function() {
             done();
           });
         },
-        setHeader: function(k, v) {
-          headers[k] = v;
-        }
+        setHeader: (key, value) => {
+          headers[key] = value;
+        },
       };
 
-      mw(req, res, next);
+      middleware(req, res, next);
     });
 
-    it('should call next for unknown file', function(done) {
-      var mw = cjs({ baseUrl: '/views', baseDir: path.join(__dirname, 'views') });
-      var req = { originalUrl: '/views/blah.html' };
-      var res = { end: function(html) {
-        throw new Error('Code shouldn\'t reach here');
-      }, send: function(code) {
-        throw new Error('Code shouldn\'t reach here');
-      }};
+    it('should call next for unknown file', (done) => {
+      const req = { originalUrl: '/views/blah.html' };
+      const res = {
+        end: () => { throw new Error('Code shouldn\'t reach here'); },
+        send: () => { throw new Error('Code shouldn\'t reach here'); },
+      };
 
-      mw(req, res, done);
+      middleware(req, res, done);
     });
 
-    it('should ignore requests without an appropriate .jade file', function(done) {
-      var mw = cjs({ baseUrl: '/views', baseDir: path.join(__dirname, 'views') });
-      var req = { originalUrl: '/views/no_tpl.css' };
-      var res = { end: function(html) {
-        throw new Error('Code shouldn\'t reach here');
-      }, send: function(code) {
-        throw new Error('Code shouldn\'t reach here');
-      }};
-      mw(req, res, done);
+    it('should ignore requests without an appropriate .pug file', (done) => {
+      const req = { originalUrl: '/views/no_tpl.css' };
+      const res = {
+        end: () => { throw new Error('Code shouldn\'t reach here'); },
+        send: () => { throw new Error('Code shouldn\'t reach here'); },
+      };
+
+      middleware(req, res, done);
     });
 
-    it('should raise error if jade template invalid', function(done) {
-      var mw = cjs({ baseUrl: '/views', baseDir: path.join(__dirname, 'views') });
-      var req = { originalUrl: '/views/tpl_err.html' };
-      var res = { end: function(html) {
-        throw new Error('Code shouldn\'t reach here');
-      }};
+    it('should raise error if pug template invalid', (done) => {
+      const req = { originalUrl: '/views/tpl_err.html' };
+      const res = {
+        end: () => {
+          throw new Error('Code shouldn\'t reach here');
+        },
+      };
 
-      mw(req, res, function(err) {
+      middleware(req, res, (err) => {
         assert.ok(err instanceof TypeError);
         done();
       });
     });
-    it('should honor the maxAge option', function(done) {
-      var mw = cjs({
+
+    it('should honor the maxAge option', (done) => {
+      middleware = connectPugStatic({
         baseUrl: '/views',
         baseDir: path.join(__dirname, 'views'),
-        maxAge: 5432
+        maxAge: 5432,
       });
-      var exp = new Date((+new Date()) + 5432).toGMTString();
-      var headers = {};
-      var req = { originalUrl: '/views/tpl.html' };
-      var res = {
-        end: function(html) {
-          // otherwise jade tries to catch this error :/
-          process.nextTick(function() {
+      const exp = new Date((+new Date()) + 5432).toGMTString();
+      const headers = {};
+      const req = { originalUrl: '/views/tpl.html' };
+      const res = {
+        end: () => {
+          // otherwise pug tries to catch this error :/
+          process.nextTick(() => {
             assert.equal(headers['Cache-Control'], 'max-age=5432');
             assert.equal(headers.Expires, exp);
             done();
           });
         },
-        setHeader: function(k, v) {
-          headers[k] = v;
-        }
+        setHeader: (key, value) => {
+          headers[key] = value;
+        },
       };
 
-      mw(req, res, next);
+      middleware(req, res, next);
     });
   });
 
 
-  describe('getTplPath()', function() {
-
-    var baseDir = path.join(__dirname, 'views');
-    var opts = {baseUrl: '/testing', baseDir: baseDir, ext: ['.jade'], allowedExt: ['.html', '.htm'], serveIndex: true};
-    var minimalOpts = {baseUrl: '/testing', baseDir: baseDir};
-
-    var helper = function(url) {
-      return cjs.getTplPath(url, opts);
+  describe('getTplPath()', () => {
+    const opts = {
+      baseUrl: '/testing',
+      baseDir,
+      ext: ['.pug'],
+      allowedExt: ['.html', '.htm'],
+      serveIndex: true,
     };
+    const minimalOpts = { baseUrl: '/testing', baseDir };
 
-    it('should work fine with basic path', function() {
-      assert.equal(helper('/testing/foo/bar.html'), baseDir + '/foo/bar.jade');
+    const helper = (url) => connectPugStatic.getTplPath(url, opts);
+
+    it('should work fine with basic path', () => {
+      assert.equal(helper('/testing/foo/bar.html'), `${baseDir}/foo/bar.pug`);
     });
 
-    it('should allow all allowed extensions', function() {
-      assert.equal(helper('/testing/foo/bar.htm'), baseDir + '/foo/bar.jade');
-      assert.equal(helper('/testing/foo/bar.html'), baseDir + '/foo/bar.jade');
+    it('should allow all allowed extensions', () => {
+      assert.equal(helper('/testing/foo/bar.htm'), `${baseDir}/foo/bar.pug`);
+      assert.equal(helper('/testing/foo/bar.html'), `${baseDir}/foo/bar.pug`);
     });
 
-    it('should reject not allowed extensions', function () {
+    it('should reject not allowed extensions', () => {
       assert.equal(helper('/testing/foo/bar.docx'), null);
     });
 
-    it('should allow all allowed extensions with minimal options', function() {
-      assert.equal(cjs.getTplPath('/testing/foo/bar.htm', minimalOpts), baseDir + '/foo/bar.jade');
-      assert.equal(cjs.getTplPath('/testing/foo/bar.html', minimalOpts), baseDir + '/foo/bar.jade');
+    it('should allow all allowed extensions with minimal options', () => {
+      assert.equal(connectPugStatic.getTplPath('/testing/foo/bar.htm', minimalOpts), `${baseDir}/foo/bar.pug`);
+      assert.equal(connectPugStatic.getTplPath('/testing/foo/bar.html', minimalOpts), `${baseDir}/foo/bar.pug`);
     });
 
-    it('should reject not allowed extensions with minimal options', function () {
-      assert.equal(cjs.getTplPath('/testing/foo/bar.docx', minimalOpts), null);
+    it('should reject not allowed extensions with minimal options', () => {
+      assert.equal(connectPugStatic.getTplPath('/testing/foo/bar.docx', minimalOpts), null);
     });
 
-    it('should work fine with path with params', function() {
-      assert.equal(helper('/testing/foo/baz.html?one=two&three=3'), baseDir + '/foo/baz.jade');
-      assert.equal(helper('/testing/foo/baz.html?kraken-here'), baseDir + '/foo/baz.jade');
+    it('should work fine with path with params', () => {
+      assert.equal(helper('/testing/foo/baz.html?one=two&three=3'), `${baseDir}/foo/baz.pug`);
+      assert.equal(helper('/testing/foo/baz.html?kraken-here'), `${baseDir}/foo/baz.pug`);
     });
 
-    it('should work fine with path with search hash', function () {
-      assert.equal(helper('/testing/foo/baz.html#menu'), baseDir + '/foo/baz.jade');
+    it('should work fine with path with search hash', () => {
+      assert.equal(helper('/testing/foo/baz.html#menu'), `${baseDir}/foo/baz.pug`);
     });
 
-    it('should return index.jade for a directory', function () {
-      assert.equal(helper('/testing/foo/'), baseDir + '/foo/index.jade');
+    it('should return index.pug for a directory', () => {
+      assert.equal(helper('/testing/foo/'), `${baseDir}/foo/index.pug`);
     });
 
-    it('should return index.jade for root', function () {
-      assert.equal(cjs.getTplPath('/', {baseUrl: '/', baseDir: baseDir + '/foo'}), baseDir + '/foo/index.jade');
+    it('should return index.pug for root', () => {
+      assert.equal(connectPugStatic.getTplPath('/', { baseUrl: '/', baseDir: `${baseDir}/foo` }),
+        `${baseDir}/foo/index.pug`);
     });
 
-    it('should return index.jade for a directory whithout trailing slash', function () {
-      assert.equal(helper('/testing/foo'), baseDir + '/foo/index.jade');
+    it('should return index.pug for a directory whithout trailing slash', () => {
+      assert.equal(helper('/testing/foo'), `${baseDir}/foo/index.pug`);
     });
 
-    it('should not return index.jade for a directory if disabled', function () {
-      assert.equal(cjs.getTplPath('/testing/foo/', {baseUrl: '/testing', baseDir: baseDir, serveIndex: false}), null);
+    it('should not return index.pug for a directory if disabled', () => {
+      assert.equal(connectPugStatic.getTplPath('/testing/foo/', {
+        baseUrl: '/testing',
+        baseDir,
+        serveIndex: false,
+      }), null);
     });
   });
-
 });
